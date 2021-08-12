434f73b50ec357633cb4e2298fd41d1ab02d72cf
student <student@ip-172-31-50-42.us-west-2.compute.internal>
2021-08-11 19:58:35 -0700

    Completed Greeter
    Completed MadLibs
    
    Completed World Domination
    
    Completed Quiz Game
    
    Awesome or Not in Progress

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -1,18 +1,21 @@
 package _03_print_and_popups._3_world_domination;
 
-
 import javax.swing.JOptionPane;
 
 public class WorldDomination {
-	
-	public static void main(String[] args) {
-		
-		// 1. Ask the user if they know how to write code.
 
+	public static void main(String[] args) {
+
+		// 1. Ask the user if they know how to write code.
+		String input = JOptionPane.showInputDialog("Do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
+		if (input.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "Good for you!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Sign up at The Leage of Amazing Programmers");
+		}
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
 }
-
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,22 +1,45 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+				String answer = JOptionPane.showInputDialog("What computer language is this program written in?");
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if(answer.equalsIgnoreCase("Java")) {
+					score += 1;
+				}else {
+					score -= 1;
+				}
 				// 4.  if the user's answer was correct, add one to their score 
-		
+		String answer2 = JOptionPane.showInputDialog("When was the original Xbox realeased?");
+		if(answer2.equals("2001")) {
+			score += 1;
+		}else {
+			score -= 1;
+		}
+		String answer3 = JOptionPane.showInputDialog("What kind of animal is a squid?");
+		if(answer3.equalsIgnoreCase("Mollusk")) {
+			score += 1;
+		}else {
+			score -= 1;
+		}
+		String answer4 = JOptionPane.showInputDialog("Are marshmelloes vegan?");
+		if(answer4.equalsIgnoreCase("No")) {
+			score += 1;
+		}else {
+			score -= 1;
+		}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
-		
+		JOptionPane.showMessageDialog(null, "Your score was a " + score + " out of a possible 4.");
 		// After all the questions have been asked, tell the user their final score 
 		
 	}
